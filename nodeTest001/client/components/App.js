748993e5e92cfd2b0a73f180a9373df50ddca61f
NeverStopLearning <basden.gerald@gmail.com>
2018-05-09 01:28:23 -0400

    complete restart due to outdated webpack instructions on tutorial. Going to set up the new env first

---
+++ nodeTest001/client/components/App.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+
+//es6 class syntex
+class App extends Component {
+	
+	
+	render(){
+		return (
+				<div>
+					App connected
+				</div>
+		)
+	}
+	
+}
+
+export default App;
